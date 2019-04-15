9a7856175d8e107ef48a198ca188b0f66e48868c
Brad Johnson <bjohnson@epgins.com>
2019-04-15 11:02:40 -0500

    quick setup

--- public/js/app.js
+++ public/js/app.js
@@ -49427,8 +49427,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Brad/freelance/powellsmythe/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Brad/freelance/powellsmythe/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Brad/Further/powellsmythe/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Brad/Further/powellsmythe/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
