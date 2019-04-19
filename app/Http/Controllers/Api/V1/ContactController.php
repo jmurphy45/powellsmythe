d5e8761cc86a8b5fa3bf43c20f4d98255a81428f
Jay Murphy <jmurphy@epgins.com>
2019-04-18 19:37:26 -0500

    make controller for contact

---
+++ app/Http/Controllers/Api/V1/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\ContactUsRequest;
+use App\Http\Controllers\Controller;
+
+class ContactController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactUsRequest $request)
+    {
+        //
+    }
+}
