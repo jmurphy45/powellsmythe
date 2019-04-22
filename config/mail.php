2d7dd77e33043993d2d628234d53f84f274cba71
Jay Murphy <jmurphy@epgins.com>
2019-04-22 11:39:29 -0500

    stuff on mail config

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'contact@powellsmythe.com'),
-        'name' => env('MAIL_FROM_NAME', 'Powell Smythe Contact'),
+        'name' => env('MAIL_FROM_NAME', 'Powell Smythe'),
     ],
 
     /*
