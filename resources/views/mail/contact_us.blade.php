2470e153a15a8f6c01b862675fec51da8a74b608
Brad Johnson <bjohnson@epgins.com>
2019-04-22 12:36:18 -0500

    mail work

--- resources/views/mail/contact_us.blade.php
+++ resources/views/mail/contact_us.blade.php
@@ -1,5 +1,4 @@
 @component('mail::message')
-{!! Html::image('https://drive.google.com/file/d/1yI1y4wkIi9DGGeWUycX_XKzAa991ORGN/view?usp=sharing') !!}
 Name: {{ $data->name }}<br />
 Email: {{ $data->email }}<br />
 Phone: {{ $data->phone }}<br />
