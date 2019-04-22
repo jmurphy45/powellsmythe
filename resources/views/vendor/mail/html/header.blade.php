e15f5fd39eb363b6dcbcdc11a2cf52f480ddbb9b
Brad Johnson <bjohnson@epgins.com>
2019-04-22 14:27:09 -0500

    mail work

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ $url }}">
             {{--{{ $slot }}--}}
 
-             <img src="{{URL::asset('/image/email-header.jpg')}}" alt="profile Pic" height="475" width="25" class="img-fluid d-block">
+             <img src="{{URL::secure_asset('/images/email-header.jpg')}}" alt="profile Pic" width="325" width="18" class="img-fluid d-block">
 
             
             {{-- <svg viewBox="0 0 491 25.13" width="475" height="25" style="fill: #404041">
